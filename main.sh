b1710f1f566288073b240b1e8d19ceed3febdee2
一只鬆 <yzsong06@outlook.com>
2023-07-27 14:35:49 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,8 @@
+#Chromimum启动指令调谐
+export PUPPETEER_EXECUTABLE_PATH=$(which chromium)
 cd Miao-Yunzai
+#启动Chromium服务
+which chromium
 #启动Redis服务
 redis-server &  --port 6379
 #启动Miao-Yunzai（此处也可使用pnpm start使项目后台运行）
